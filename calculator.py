d669e972be9969e52c0a02eb289b6a8f51b3a245
Mario Juric <mjuric@lsst.org>
2015-02-02 06:11:54 -0800

    Basic scaffolding for the calculator code
    Implemented the basic scaffolding, such as parsing the command line and
    returning an error if it's malformed. The is still no functionality, as no
    arithmetic operators are recognized and acted upon.

---
+++ calculator.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import sys
+
+def main():
+	# Parse the command line
+	try:
+		a, op, b = sys.argv[1].split()
+		a, b = float(a), float(b)
+	except:
+		print 'Usage: calculator.py "A op B"'
+		return -1
+
+	print 'Unknown operator', op
+	return -1
+
+if __name__ == "__main__":
+	main()
